cc3b719a21ddc8328d1620b08693eff172010f7c
Herve R. Munyurangeri <rherve@gmail.com>
2024-11-19 13:17:34 -0600

    fix: logger middleware

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,14 +1,21 @@
 import status from "http-status";
 import logger from "../utils/logger.js";
+import { toSnakeCase } from "../utils/convert-case.js";
 
 export const requestLogger = (req, res, next) => {
   logger.info(`${new Date().toISOString()}: ${req.method} ${req.url}`);
   next();
 };
 
-export const errorLogger = (err, req, res) => {
+export const errorLogger = (err, req, res, next) => {
   logger.error(err.message);
-  res
-    .status(status.INTERNAL_SERVER_ERROR)
-    .json({ error: "Internal Server Error" });
+
+  if (err.message.startsWith("Validation"))
+    res
+      .status(status.BAD_REQUEST)
+      .json({ status: "bad request", message: toSnakeCase(err.message) });
+  else
+    res
+      .status(status.INTERNAL_SERVER_ERROR)
+      .json({ status: "Internal Server Error", error: err.message });
 };
