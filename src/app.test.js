ff0f39403c2b7c1ac4d20c10b33c2303784f51a0
Herve R. Munyurangeri <rherve@gmail.com>
2024-11-19 13:20:26 -0600

    feat: update users routes

--- src/app.test.js
+++ src/app.test.js
@@ -15,21 +15,53 @@ describe("API", () => {
 
   describe("Users", () => {
     it("should return all users", async () => {
+      const data = {
+        first_name: "Caka",
+        last_name: "Umutoni",
+        email: "caka.umutoni@fakemail.com",
+        city: "Oklahoma",
+        country: "USA",
+      };
+
+      await request(app).post("/api/users").send(data);
+      await request(app).post("/api/users").send(data);
+      await request(app).post("/api/users").send(data);
+
       const response = await request(server)
         .get("/api/users")
-        .query({ _page: 2, _per_page: 20, _search: "dupond dupont" });
+        .query({ _page: 1, _per_page: 1, _search: "dupond dupont" });
 
       console.log(response.body);
 
       expect(response.status).toBe(status.OK);
+      expect(response.body.next).toBe(2);
+      Object.keys(data).forEach((key) => {
+        expect(response.body.data[0]).toHaveProperty(key);
+      });
     });
 
     it("should add a new user", async () => {
-      const response = await request(app)
-        .post("/api/users")
-        .send({ name: "Umutoni Caka" });
+      const data = {
+        first_name: "Caka",
+        last_name: "Umutoni",
+        email: "caka.umutoni@fakemail.com",
+        city: "Oklahoma",
+        country: "USA",
+      };
+
+      const response = await request(app).post("/api/users").send(data);
 
       expect(response.status).toBe(status.CREATED);
+      Object.keys(data).forEach((key) => {
+        expect(response.body.user).toHaveProperty(key);
+      });
+    });
+
+    it("should NOT add a new user IF any REQUIRED field is missing", async () => {
+      const response = await request(app).post("/api/users").send({});
+
+      expect(response.status).toBe(status.BAD_REQUEST);
+      expect(response.body.message).toContain('"first_name"');
     });
   });
 });
--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,12 +1,16 @@
 import express from "express";
 import authorize from "../middleware/authorization.js";
-import { getAllUsers, createUser } from "../controllers/users.js";
+import createUserRepository from "../adaptors/in-memory-db/user.js";
+import createUserService from "../core/user/service.js";
+import createUserController from "../adaptors/http/users.js";
 
 const ROLES = ["guest"];
-
 const router = express.Router();
+const userRepository = createUserRepository();
+const userService = createUserService(userRepository);
+const { getAllUsers, addUser } = createUserController(userService);
 
 router.get("/", authorize([...ROLES, "admin", "user"]), getAllUsers);
-router.post("/", authorize([...ROLES, "admin"]), createUser);
+router.post("/", authorize([...ROLES, "admin"]), addUser);
 
 export default router;
